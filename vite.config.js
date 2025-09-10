645ee256590828c65572c1ae072972018037e09c
DEEPAK T N <134320743+DeepakDeeputn@users.noreply.github.com>
2025-09-10 22:02:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // Replace <repo-name> with your GitHub repo name
+  base: './', // Replace <repo-name> with your GitHub repo name
 })
